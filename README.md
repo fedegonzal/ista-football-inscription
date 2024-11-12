e7b34d568bcafd46b7d13f23ca11a1ef568aef9f
Federico Gonzalez <federico.gonzalez@ist.ac.at>
2024-11-12 10:24:13 +0100

    First version!

---
+++ README.md
@@ -0,0 +1,20 @@
+# ISTA football automated inscription
+
+Since many people waits hundreds of hours to get a spot in the next football game, I decided to automate the inscription process. This script will automatically inscribe you in the next game once the inscription is open, without any effort.
+
+## How to use
+
+Prefered with Chrome, but it should work with any browser (I didn't test it with other browsers).
+
+Do it every Fridat at 7:00 AM before the inscription opens.
+
+1. Open WhatsApp Web (the web version, not the app)
+2. Open the chat with the ISTA football group
+3. Open the console (right click -> inspect -> console)
+4. Paste the code from index.js and press enter
+
+Do not change the chat to another chat until the inscription is done. If you want to use WhatsApp while the script is running, use your phone!
+
+## Warranty
+
+No warranty is provided. Use at your own risk. I am not responsible for any damage caused by this script (mainly hurting someone's feelings because you took their spot).
---
+++ index.js
@@ -0,0 +1,65 @@
+(function monitorWhatsApp() {
+    console.log("Monitor Started...");
+
+    // List of messages to react 
+    // when someone sends a message that includes any of these words
+    // in the current chat, the script will react replying with the reaction
+    const reactTo = ["+1", "+ 1"];
+
+    // The reaction to send. We accept emojis too!!
+    const reaction = "+1";
+    
+    // This variable will help us to avoid sending the reaction multiple times
+    let reacted = false;
+
+    function sendMessage(text) {
+        // Find the message input box
+        const messageBox = document.querySelector(
+            'footer div[contenteditable="true"]'
+        );
+        if (messageBox) {
+            // Focus on the input box
+            messageBox.focus();
+
+            // We need to create a new event to trigger the input event
+            // This is necessary because WhatsApp uses React 
+            // and the input event is not triggered by simply 
+            // setting the value of the input box
+            const event = new InputEvent("input", {
+                bubbles: true,
+                cancelable: true,
+                data: text,
+                inputType: "insertText",
+            });
+            messageBox.dispatchEvent(event);
+
+            // Wait a moment and then send the message
+            setTimeout(() => {
+                const sendButton = document.querySelector(
+                    'span[data-icon="send"]'
+                );
+                if (sendButton) sendButton.click();
+            }, 100);
+        }
+    }
+
+    // This function will check the last message JUST in the current chat
+    function checkLastMessage() {
+        const messages = document.querySelectorAll(
+            "div.message-in span.selectable-text"
+        );
+        if (messages.length > 0) {
+            const lastMessage = messages[messages.length - 1].innerText.trim();
+
+            // If the last message is in the reactTo array
+            // and we haven't reacted to it yet
+            if (reactTo.some(item => lastMessage.includes(item)) && !reacted) {
+                sendMessage(reaction);
+                reacted = true;
+            }
+        }
+    }
+
+    // Check for new messages every 2 second
+    setInterval(checkLastMessage, 2 * 1000);
+})();
